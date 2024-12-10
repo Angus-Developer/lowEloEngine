d87a11c4daef037ed27629e0968a3c1c0dcaa8af
Angus-Developer <angusbrowning.switch@gmail.com>
2024-12-10 20:04:32 +1100

    Pawn moves

--- ChessEngine.py
+++ ChessEngine.py
@@ -17,6 +17,8 @@ class GameState():
             ['--', '--', '--', '--', '--', '--', '--', '--'],
             ['wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp'],
             ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR']]
+        self.moveFunctions = {'p': self.getPawnMoves, 'R': self.getRookMoves, 'N': self.getKnightMoves, 'B': self.getBishopMoves, 'Q': self.getQueenMoves, 'K': self.getKingMoves}
+
         self.whiteToMove = True
         self.moveLog = []
 
@@ -53,19 +55,28 @@ class GameState():
         for r in range(len(self.board)): # number of rows
             for c in range(len(self.board[r])): # Number of cols in given row
                 turn = self.board[r][c][0]
-                if (turn == 'w' and self.whiteToMove) and (turn == 'b' and not self.whiteToMove):
+                if (turn == 'w' and self.whiteToMove) or (turn == 'b' and not self.whiteToMove):
                     piece = self.board[r][c][1]
-                    if piece == 'p':
-                        self.getPawnMoves(r, c, moves)
-                    elif piece == 'R':
-                        self.getRookMoves(r, c, moves)
+                    self.moveFunctions[piece](r, c, moves) # Calls the appropriate move function based on piece type
         return moves
     
     '''
     Get all the pawn moves for the pawn located at row, col and ad these moves to the list
     '''
     def getPawnMoves(self, r, c, moves):
-        pass
+        if self.whiteToMove: # White pawn moves
+            if self.board[r-1][c] == '--': # 1 square pawn advance
+                moves.append(Move((r, c), (r-1, c), self.board))
+                if r == 6 and self.board[r-2][c] == '--': # 2 square pawn advances
+                    moves.append(Move((r, c), (r-2, c), self.board))
+            if c-1 >= 0: # Captures to the left
+                if self.board[r-1][c-1] == 'b': # Enemy piece to capture
+                    moves.append(Move((r, c), (r-1, c-1), self.board))
+            if c+1 <= 7: # Captures to the right
+                if self.board[r-1][c+1][0] == 'b':
+                    moves.append(Move((r, c), (r-1, c+1), self.board))
+        else: # Black pawn moves
+            pass
 
     '''
     Get all the rook moves for the rook located at row, col and ad these moves to the list
@@ -73,6 +84,29 @@ class GameState():
     def getRookMoves(self, r, c, moves):
         pass
 
+    '''
+    Get all the knight moves for the knight located at row, col and ad these moves to the list
+    '''
+    def getKnightMoves(self, r, c, moves):
+        pass
+
+    '''
+    Get all the bishop moves for the bishop located at row, col and ad these moves to the list
+    '''
+    def getBishopMoves(self, r, c, moves):
+        pass
+
+    '''
+    Get all the queen moves for the queen located at row, col and ad these moves to the list
+    '''
+    def getQueenMoves(self, r, c, moves):
+        pass
+
+    '''
+    Get all the king moves for the king located at row, col and ad these moves to the list
+    '''
+    def getKingMoves(self, r, c, moves):
+        pass
 
 class Move():
     # Map keys to values
@@ -90,8 +124,6 @@ class Move():
         self.pieceMoved = board[self.startRow][self.startCol]
         self.pieceCaptured = board[self.endRow][self.endCol]
         self.moveID = self.startRow * 1000 + self.startCol * 100 + self.endRow * 10 + self.endCol
-        print(self.moveID)
-
     '''
     Overriding the equals method
     '''
